f0c2d0b673bca30f5aaf7b3cf4f8e42ce4d71023
lunaksj <lunaksj@naver.com>
2022-02-01 01:38:35 +0900

    Lesson 5. SwiftUI War Card Game UI Construction

---
+++ war-challenge/war-challenge/ContentView.swift
@@ -0,0 +1,64 @@
+// 내가 그냥 짠 코드
+//
+//  ContentView.swift
+//  war-challenge
+//
+//  Created by Sunji Kim on 2022/02/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Image("background")
+                            .edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                                        Spacer()
+                    Image("card3")
+                                        Spacer()
+                    Image("card4")
+                                        Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                                                    .foregroundColor(Color.white)
+                        Text("0")
+                            .font(.largeTitle)
+                                                    .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                            .foregroundColor(Color.white)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
